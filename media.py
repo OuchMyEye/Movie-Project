6598681b4128070a9011c3d2c546ac4be91fc1e7
jlarios <jamie.larios@gmail.com>
2017-01-24 19:17:47 -0800

    Add docstrings to class and init

--- media.py
+++ media.py
@@ -1,13 +1,14 @@
 import webbrowser
 
-# define Movie class
 class Movie():
+    """ This class defines the structure for storing movie information """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """ This init constructor creates the instance variables """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-# define show_trailer function
+    # define show_trailer function
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
